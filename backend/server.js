73aed19451f2217ef9b93f855feef32bfe8ee756
Reshma Rajesh <reshmarajesh28@gmail.com>
2025-11-08 23:17:11 +0530

    Fixed backend syntax error and verified app is working

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_URL, {
+
       useNewUrlParser: true,
   useUnifiedTopology: true,
 })
