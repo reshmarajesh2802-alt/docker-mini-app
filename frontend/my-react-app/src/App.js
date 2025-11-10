3e444cbfb214446f8191f52fe184ff142df2e17f
Reshma Rajesh <reshmarajesh28@gmail.com>
2025-11-10 22:18:56 +0530

    Recently updated front end App.js

--- frontend/my-react-app/src/App.js
+++ frontend/my-react-app/src/App.js
@@ -1,21 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  const [setMessage, setMessage] = useState("");  
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
-  fetch("https://docker-mini-backend-jmvp.onrender.com/api/hello") // <-- This is the line to change
-    .then(res => res.json())
-
-    .then(text => setMessage(text)) 
-
-    .catch(err => console.error(err));
-}, []);  //temporary commit
-
+    fetch("http://localhost:5000/api/hello")
+      .then(res => res.json())
+      .then(data => setMessage(data.message));
+  }, []);
 
   return (
-    <div style={{ textAlign: "center", marginTop: "3rem" }}>
-      <h1>Frontend + Backend + DB with Docker</h1>
+    <div style={{ textAlign: 'center', marginTop: '3rem' }}>
+      <h1>Frontend + Backend + DB with Docker 🐳</h1>
       <p>Message from backend: <b>{message}</b></p>
     </div>
   );
