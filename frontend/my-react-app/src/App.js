efb86edbfb5eed30a63b85a7451614cc1458337e
Reshma Rajesh <reshmarajesh28@gmail.com>
2025-11-10 22:20:01 +0530

    Recently changed  front end App.js

--- frontend/my-react-app/src/App.js
+++ frontend/my-react-app/src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/hello")
+    fetch("https://docker-mini-backend-jmvp.onrender.com/api/hello")
       .then(res => res.json())
       .then(data => setMessage(data.message));
   }, []);
